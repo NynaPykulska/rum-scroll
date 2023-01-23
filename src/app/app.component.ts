0f893fc4483597c2826c16ab47a4c298cf8f3f99
Nina Pikulska <nipi@konsolidator.com>
2023-01-23 10:53:04 +0100

    Table implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {datadogRum} from "@datadog/browser-rum";
+import {TreeNode} from "primeng/api";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rum-scroll';
+export class AppComponent implements OnInit {
+  files1: TreeNode[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getDataFromFile().then(files => this.files1 = files);
+
+
+    datadogRum.init({
+      applicationId: '085e3340-508d-44ba-89e4-83f663d522a2',
+      clientToken: 'pub51bf97289acd53debf656c966d521287',
+      site: 'datadoghq.eu',
+      service: 'konsolidatorui',
+      version: '0.0.0',
+      env: 'test',
+      sampleRate: 100,
+      trackInteractions: true,
+      allowedTracingOrigins: ['https://uiapi.test.konsolidator.com/api'],
+    });
+    // datadogRum.startSessionReplayRecording();
+  }
+
+  getDataFromFile() {
+    return this.http.get<any>('assets/data.json')
+      .toPromise()
+      .then(res => <TreeNode[]>res.data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TreeTableModule} from "primeng/treetable";
+import {TableModule} from "primeng/table";
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {DialogModule} from 'primeng/dialog';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ToastModule} from 'primeng/toast';
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TreeTableModule,
+    TableModule,
+    ScrollingModule,
+    ToastModule,
+    DialogModule,
+    ButtonModule,
+    MultiSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
